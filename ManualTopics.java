a24abc8e3a46eaccf01282f4df426e94a4fc6041
Tanuja Mistry <mistry_tanuja@yahoo.com>
2024-04-22 22:46:39 +0100

    my first homework in java

---
+++ ManualTopics.java
@@ -0,0 +1,50 @@
+package listofmanualtopics;
+
+public class ManualTopics {
+
+    public static void main(String[] args){
+
+        System.out.println("list of Manual testing topics:");
+        System.out.println("1. What is Software Testing?");
+        System.out.println("2. Benefits of Software Testing");
+        System.out.println("3. Who does Testing?");
+        System.out.println("4. Qualification of Tester");
+        System.out.println("5. Challenges of Tester");
+        System.out.println("6. What is Manual Testing?");
+        System.out.println("7. Software Development Life Cycle");
+        System.out.println("8. Software Testing Life Cycle");
+        System.out.println("9. Challenges of Manual Tester");
+        System.out.println("10. Principles of Software Testing");
+        System.out.println("11. What is Test Plan?");
+        System.out.println("12. Why is it important to have Test Plan?");
+        System.out.println("13. Test Plan Template");
+        System.out.println("14. What is Test Scenario?");
+        System.out.println("15. Why do we write Test Scenario?");
+        System.out.println("16. Test Scenario Template");
+        System.out.println("17. What is Test Case?");
+        System.out.println("18. Why do we write Test Case?");
+        System.out.println("19. Positive Testing");
+        System.out.println("20. Negative Testing");
+        System.out.println("21. Test Case Template");
+        System.out.println("22. Requirement Traceability Matrix");
+        System.out.println("23. Test Scenario VS Test Case");
+        System.out.println("24. Software Testing Techniques:");
+        System.out.println("25. Equivalence Class Partitioning");
+        System.out.println("26. Boundary Value Analysis");
+        System.out.println("27. Error Guessing");
+        System.out.println("28. What is Bug?");
+        System.out.println("29. Why are there bugs in the software");
+        System.out.println("30. Bugs Life Cycle");
+        System.out.println("31. Bugs Template");
+        System.out.println("32. Severity VS priority");
+        System.out.println("33. Black Box Testing VS White Box Testing");
+        System.out.println("34. Testing levels");
+        System.out.println("35. System Testing");
+        System.out.println("36. Functional Testing Vs Non-Functional Testing");
+        System.out.println("37. Smoke Testing VS Sanity Testing");
+        System.out.println("38. Regression Testing Vs Re-testing");
+        System.out.println("39. User Acceptance Testing");
+        System.out.println("40. Alpha Testing Vs Beta Testing");
+        System.out.println("41. Static Testing Vs Dynamic Testing");
+    }
+}
